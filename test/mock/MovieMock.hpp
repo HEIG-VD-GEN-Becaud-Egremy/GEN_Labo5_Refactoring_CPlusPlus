a6a52500a701eb4a6dfac94b6ba9bd199a9435c7
BecaudA <arthur.becaud@heig-vd.ch>
2020-06-16 22:29:56 +0200

    Created Movie Mock file and linked it to CMake executable

---
+++ test/mock/MovieMock.hpp
@@ -0,0 +1,15 @@
+#ifndef LABO5_MOVIEMOCK_HPP
+#define LABO5_MOVIEMOCK_HPP
+
+#include <gmock/gmock.h>
+#include "../../src/Movie.h"
+
+class MovieMock : public Movie {
+public:
+    MOCK_METHOD(std::string, getTitle,     (),            (const));
+    MOCK_METHOD(double,      getPrice,     (unsigned int),(const));
+    MOCK_METHOD(int,         getPriceCode, (),            (const));
+};
+
+
+#endif //LABO5_MOVIEMOCK_HPP
