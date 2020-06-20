7f4fce6e07ccc854a037bc246a0aaf97cbc5bcc3
BecaudA <arthur.becaud@heig-vd.ch>
2020-06-20 09:57:37 +0200

    Fix a small errror in EXPECT_CALL statement of a test.

--- test/RentalTest.cpp
+++ test/RentalTest.cpp
@@ -9,54 +9,50 @@
 using ::testing::Return;
 
 TEST(RentalGetPrice, unit) {
-    MovieMock movie;
+    std::shared_ptr<MovieMock> movieMock = std::make_shared<MovieMock>();
 
-    EXPECT_CALL(movie, getPrice(2)).WillRepeatedly(Return(2.));
-    EXPECT_CALL(movie, getPrice(4)).WillRepeatedly(Return(5.));
+    EXPECT_CALL(*movieMock, getPrice(2)).WillRepeatedly(Return(2.));
+    EXPECT_CALL(*movieMock, getPrice(4)).WillRepeatedly(Return(5.));
 
-    Rental rental2Days = Rental(&movie, 2);
-    Rental rental4Days = Rental(&movie, 4);
+    Rental rental2Days = Rental(movieMock, 2);
+    Rental rental4Days = Rental(movieMock, 4);
 
     ASSERT_DOUBLE_EQ(rental2Days.getPrice(), 2.);
     ASSERT_DOUBLE_EQ(rental4Days.getPrice(), 5.);
 }
 
-TEST(RentalGetRenderPoints, simple) {
-    MovieMock regularMovieMock;
-    MovieMock newReleaseMovie1Mock;
-    MovieMock newReleaseMovie2Mock;
-
-    Movie* regularMovie     = &regularMovieMock;
-    Movie* newReleaseMovie1 = &newReleaseMovie1Mock;
-    Movie* newReleaseMovie2 = &newReleaseMovie2Mock;
+TEST(RentalGetRenterPoints, simple) {
+    std::shared_ptr<MovieMock> regularMovieMock = std::make_shared<MovieMock>();
+    std::shared_ptr<MovieMock> newReleaseMovie1Mock = std::make_shared<MovieMock>();
+    std::shared_ptr<MovieMock> newReleaseMovie2Mock = std::make_shared<MovieMock>();
 
     std::shared_ptr<PriceCodeRegularMock>    pcRegularMockPtr    = std::make_shared<PriceCodeRegularMock>();
     std::shared_ptr<PriceCodeNewReleaseMock> pcNewReleaseMockPtr = std::make_shared<PriceCodeNewReleaseMock>();
 
-    EXPECT_CALL(regularMovieMock,     getPriceCode()).WillRepeatedly(Return(pcRegularMockPtr));
-    EXPECT_CALL(newReleaseMovie1Mock, getPriceCode()).WillRepeatedly(Return(pcNewReleaseMockPtr));
-    EXPECT_CALL(newReleaseMovie2Mock, getPriceCode()).WillRepeatedly(Return(pcNewReleaseMockPtr));
+    EXPECT_CALL(*regularMovieMock,     getPriceCode()).WillRepeatedly(Return(pcRegularMockPtr));
+    EXPECT_CALL(*newReleaseMovie1Mock, getPriceCode()).WillRepeatedly(Return(pcNewReleaseMockPtr));
+    EXPECT_CALL(*newReleaseMovie2Mock, getPriceCode()).WillRepeatedly(Return(pcNewReleaseMockPtr));
 
-    EXPECT_CALL(*pcRegularMockPtr,    getRenderPoints(1u)).WillRepeatedly(Return(1u));
-    EXPECT_CALL(*pcNewReleaseMockPtr, getRenderPoints(1u)).WillRepeatedly(Return(1u));
-    EXPECT_CALL(*pcNewReleaseMockPtr, getRenderPoints(2u)).WillRepeatedly(Return(2u));
+    EXPECT_CALL(*pcRegularMockPtr,    getRenterPoints(1u)).WillRepeatedly(Return(1u));
+    EXPECT_CALL(*pcNewReleaseMockPtr, getRenterPoints(1u)).WillRepeatedly(Return(1u));
+    EXPECT_CALL(*pcNewReleaseMockPtr, getRenterPoints(2u)).WillRepeatedly(Return(2u));
 
-    Rental rentalRegularMovie     = Rental(regularMovie,     1);
-    Rental rentalNewReleaseMovie1 = Rental(newReleaseMovie1, 1);
-    Rental rentalNewReleaseMovie2 = Rental(newReleaseMovie2, 2);
+    Rental rentalRegularMovie     = Rental(regularMovieMock,     1);
+    Rental rentalNewReleaseMovie1 = Rental(newReleaseMovie1Mock, 1);
+    Rental rentalNewReleaseMovie2 = Rental(newReleaseMovie2Mock, 2);
 
-    ASSERT_EQ(rentalRegularMovie.getRenterPoints(),    1);
-    ASSERT_EQ(rentalNewReleaseMovie1.getRenterPoints(),1);
-    ASSERT_EQ(rentalNewReleaseMovie2.getRenterPoints(),2);
+    ASSERT_EQ(rentalRegularMovie.getRenterPoints(), 1);
+    ASSERT_EQ(rentalNewReleaseMovie1.getRenterPoints(), 1);
+    ASSERT_EQ(rentalNewReleaseMovie2.getRenterPoints(), 2);
 }
 
 TEST(RentalGetMovie, simple) {
     const std::string TITLE = "The Trembling Window";
-    MovieMock movie;
+    std::shared_ptr<MovieMock> movieMock = std::make_shared<MovieMock>();
 
-    EXPECT_CALL(movie, getTitle()).WillRepeatedly(Return(TITLE));
+    EXPECT_CALL(*movieMock, getTitle()).WillRepeatedly(Return(TITLE));
 
-    Rental rental = Rental(&movie, 2);
+    Rental rental = Rental(movieMock, 2);
 
-    ASSERT_EQ(rental.getMovie().getTitle(), TITLE);
+    ASSERT_EQ(rental.getMovie()->getTitle(), TITLE);
 }
